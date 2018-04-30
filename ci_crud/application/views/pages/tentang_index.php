eb31f76b14e0579d5fcc3f6da29915e847b54a1f
Fajar <jars712.fs@gmail.com>
2018-04-30 23:20:22 +0700

    Initial commit

---
+++ ci_crud/application/views/pages/tentang_index.php
@@ -0,0 +1,7 @@
+<div class="card card-default" style="border-radius: 0px;margin: 20px 0;">
+     <div class="card-header">Tentang Pembuat Website</div>
+     <div class="card-body">
+          <p>Nama Saya Hendri Azka Zulfiqar <br/>Project pembuatan website ini untuk memenuhi tugas Web Dinamis XI materi CRUD (Create, Read, Update & Delete).</p>
+        
+     </div>
+</div>
